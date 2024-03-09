ef473feb0796bf74391a88de7833c84c7cdeb67d
wrapper228 <32472244+wrapper228@users.noreply.github.com>
2024-03-09 17:20:52 +0300

    log what loader outputs

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import torch
 
 from options import args
-from models import model_factory
+from models.bert import BERTModel
 from dataloaders import make_dataloaders
 from trainers import trainer_factory
 from utils import *
 
 
 def train():
-    export_root = setup_train(args)
     train_loader, val_loader, test_loader = make_dataloaders(args)
 
-    model = model_factory(args)
+    model = BERTModel(args)
+
+    some_batch = next(iter(train_loader))
+    seq, _ = some_batch
+    print(seq.shape)
+    print(seq[0, ...].unsqueeze(0).shape)
+    print(model(seq[0, ...].unsqueeze(0)).shape)
+
+    export_root = setup_train(args)
     trainer = trainer_factory(args, model, train_loader, val_loader, test_loader, export_root)
     trainer.train()
+
+    # цель: вытащить этот процесс трейна наружу. первым делом понять что выплевывает model(batch)
 
     test_model = (input('Test model with test dataset? y/[n]: ') == 'y')
     if test_model:
